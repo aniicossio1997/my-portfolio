cf62b2b1b7802e31548e8c73b7a2333245bdee1e
ana cossio <anacossio7@gmail.com>
2022-02-05 10:12:51 -0300

    the layout is created

---
+++ src/layout/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Center, Container, Grid, Stack, VStack } from "@chakra-ui/react";
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/footer";
+const index = ({ children }: any) => {
+  return (
+    <>
+      <Stack h={"100%"} spacing={100}>
+        <Box mb={"50px"}>
+          <Header />
+        </Box>
+        <Stack alignItems={"center"} justifyContent={"center"}>
+          <Container maxWidth={{ base: "100%", md: "95%" }}>
+            <Stack
+              minH={"100%"}
+              display={"flex"}
+              alignContent={"center"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Center alignSelf={"center"}>{children}</Center>
+            </Stack>
+            <Footer />
+          </Container>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+
+export default index;
