6638f80a15d2def8c64ac990f597983bd89279f6
ana cossio <anacossio7@gmail.com>
2022-02-05 10:19:07 -0300

    dark mode is added to the project and styles are defined

---
+++ src/themes/theme.ts
@@ -0,0 +1,66 @@
+import { extendTheme, theme, Button } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const themeConfig = extendTheme({
+  config,
+  colors: {
+    primary: theme.colors.messenger,
+  },
+  sizes: {
+    container: {
+      xl: "1700px",
+    },
+  },
+  styles: {
+    global: (props: any) => ({
+      "html, body, #root": {
+        color: mode("gray.600", "white")(props),
+        height: "100%",
+      },
+      "*::-webkit-scrollbar": {
+        display: "none",
+      },
+    }),
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        borderLeftRadius: 9999,
+        borderRightRadius: 9999,
+      },
+    },
+  },
+  textStyles: {
+    h1: {
+      // you can also use responsive styles
+      fontSize: ["48px", "72px"],
+      fontWeight: "bold",
+      lineHeight: "110%",
+      letterSpacing: "-2%",
+    },
+    h2: {
+      fontSize: ["24px", "32px", "38px"],
+      fontWeight: "semibold",
+      lineHeight: "110%",
+      letterSpacing: "-1%",
+    },
+    h3: {
+      fontSize: ["15px", "20px", "25px"],
+      fontWeight: "semibold",
+      lineHeight: "110%",
+      letterSpacing: "-1%",
+    },
+    p: {
+      fontSize: ["12px", "15px", "18px"],
+    },
+  },
+});
+
+export default themeConfig;
