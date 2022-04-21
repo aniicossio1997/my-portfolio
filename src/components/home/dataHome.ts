71fcb4353baeb1c717cc22f7e6390e083075221a
ana cossio <anacossio7@gmail.com>
2022-04-21 15:10:27 -0300

    se modifica dataHome

--- src/components/home/dataHome.ts
+++ src/components/home/dataHome.ts
@@ -1,5 +1,5 @@
 export const dataHome = {
-  title: "!Hola :) !",
+  title: "Hola 😊",
   description: `Soy una persona apasionada por la tecnología que se encuentra en constante aprendizaje,tengo facilidad para resolver problemas analiticamente y con creatividad.,
       `,
   btnTitle: "Mis proyectos",
