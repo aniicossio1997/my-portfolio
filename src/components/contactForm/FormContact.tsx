e1e42f4839117c34696f77a00ac1e7dbbb9bc153
ana cossio <anacossio7@gmail.com>
2022-04-29 06:56:41 -0300

    se modifica el formulario de contacto y se le agrega el public key para enviar los emails

--- src/components/contactForm/FormContact.tsx
+++ src/components/contactForm/FormContact.tsx
@@ -2,7 +2,7 @@ import { Formik, Form, FormikHelpers } from "formik";
 import { Button, Input, Stack, Textarea } from "@chakra-ui/react";
 import { InputText } from "./InputText";
 import { ContactSchema, initialValues, Values } from "./dataForm";
-import { useRef, MutableRefObject, useEffect } from "react";
+import { useRef, MutableRefObject, useEffect, useState } from "react";
 import emailjs, { init } from "@emailjs/browser";
 import { DataKeyEmail } from "../../data/DataKeyEmail";
 interface InputI {
@@ -18,14 +18,23 @@ interface RefObject<T> {
 const FormContact = () => {
   emailjs.init(DataKeyEmail.user_id);
   const form = useRef<any>(""); // MutableRefObject<null>
+  const [isLoading, setIsLoading] = useState(false);
   const sendEmail = (value: Values) => {
+    console.log("enviando mensaje.....");
+    console.log(JSON.stringify(DataKeyEmail));
+    setIsLoading(true);
     emailjs
-      .send(DataKeyEmail.service_id, DataKeyEmail.template_id, {
-        name: value.name,
-        subject: value.subject,
-        message: value.menssage,
-        email: value.email,
-      })
+      .send(
+        DataKeyEmail.service_id,
+        DataKeyEmail.template_id,
+        {
+          name: value.name,
+          subject: value.subject,
+          message: value.menssage,
+          email: value.email,
+        },
+        DataKeyEmail.user_id
+      )
       .then(
         (result) => {
           console.log(result.text);
@@ -34,6 +43,7 @@ const FormContact = () => {
           console.log(error.text);
         }
       );
+    setIsLoading(false);
     // fetch("https://api.emailjs.com/api/v1.0/email/send-form", {
     //   method: "POST",
     //   headers: {
@@ -47,64 +57,70 @@ const FormContact = () => {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values: Values, { resetForm }: FormikHelpers<Values>) => {
-        sendEmail(values);
-        resetForm();
-      }}
-      validationSchema={ContactSchema}
-    >
-      {({ handleReset, errors }) => (
-        <Form noValidate ref={form}>
-          <InputText
-            label={"Email"}
-            name={"email"}
-            type="email"
-            Component={Input}
-          />
-          <InputText
-            label={"Subject"}
-            name={"subject"}
-            type="text"
-            Component={Input}
-          />
-          <InputText
-            label={"Name"}
-            name={"name"}
-            type="text"
-            Component={Input}
-          />
-          <InputText
-            label={"Menssage"}
-            name={"menssage"}
-            type="textarea"
-            Component={Textarea}
-          />
+    <>
+      {isLoading ? (
+        <h1>ENVIANDO Mensaje...</h1>
+      ) : (
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values: Values, { resetForm }: FormikHelpers<Values>) => {
+            sendEmail(values);
+            resetForm();
+          }}
+          validationSchema={ContactSchema}
+        >
+          {({ handleReset, errors }) => (
+            <Form noValidate ref={form}>
+              <InputText
+                label={"Email"}
+                name={"email"}
+                type="email"
+                Component={Input}
+              />
+              <InputText
+                label={"Subject"}
+                name={"subject"}
+                type="text"
+                Component={Input}
+              />
+              <InputText
+                label={"Name"}
+                name={"name"}
+                type="text"
+                Component={Input}
+              />
+              <InputText
+                label={"Menssage"}
+                name={"menssage"}
+                type="textarea"
+                Component={Textarea}
+              />
 
-          <Stack direction={"column"} spacing={4} mt={5}>
-            <Button
-              colorScheme="pink"
-              variant="solid"
-              type={"submit"}
-              width={"fullWidth"}
-            >
-              Send
-            </Button>
-            <Button
-              colorScheme={"gray"}
-              borderWidth={1}
-              borderColor={"gray.300"}
-              variant="solid"
-              onClick={handleReset}
-              width={"fullWidth"}
-            >
-              reset
-            </Button>
-          </Stack>
-        </Form>
+              <Stack direction={"column"} spacing={4} mt={5}>
+                <Button
+                  colorScheme="pink"
+                  variant="solid"
+                  type={"submit"}
+                  width={"fullWidth"}
+                >
+                  Send
+                </Button>
+                <Button
+                  colorScheme={"gray"}
+                  borderWidth={1}
+                  borderColor={"gray.300"}
+                  variant="solid"
+                  onClick={handleReset}
+                  width={"fullWidth"}
+                >
+                  reset
+                </Button>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
       )}
-    </Formik>
+    </>
   );
 };
 
